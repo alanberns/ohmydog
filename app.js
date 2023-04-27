0b54878335702e51d4a06a5e6d224849872df9b9
Alan Berns <alan_berns@yahoo.com.ar>
2023-04-27 11:30:43 -0300

    feat: dotenv

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //imports
 const express = require('express');
 const path = require('path');
+require("dotenv").config();
 
 
 //inicializacion
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 
 //routers
