8a09b1b9f69c75c096d92fb122ab07addeaa3ca9
Alan Berns <alan_berns@yahoo.com.ar>
2023-04-27 11:25:21 -0300

    feat: agregado BD + ORM

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
+//imports
 const express = require('express');
 const path = require('path');
 
 
+//inicializacion
 const app = express();
 const port = 3000;
 
 
+//routers
+
+
+//indicar a la app lo que debe usar
 app.use(express.static('public'));
 
 
-app.set('views',path.resolve(__dirname,'./src/views'));
 
+//view engine
+app.set('views',path.resolve(__dirname,'./src/views'));
+//app.set('view engine', 'pug');
+
+
+//rutas
 app.get('/', (req,res) => {
     res.send("Hola mundo");
 });
+
 
 
 app.listen(port, () => {
