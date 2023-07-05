48e77e6c474882e79a2bbbf5e56944aaecf6cd7e
Alan Berns <alan_berns@yahoo.com.ar>
2023-07-05 11:42:39 -0300

    admin no puede acceder a cruza

--- src/routes/cruzaRoutes.js
+++ src/routes/cruzaRoutes.js
@@ -6,7 +6,7 @@ const permisos = require('../helpers/permisos')
 router = express.Router();
 
 
-router.get('/', permisos.isAuth, cruzaController.index);
+router.get('/', permisos.esCliente, cruzaController.index);
 router.get('/cruzar/:id', permisos.esCliente, cruzaController.cruzar);
 router.post('/cruzar/ok', permisos.esCliente, cruzaController.cruzarConfirmar);
 router.get('/buscar/:id', permisos.esCliente, cruzaController.buscarPerrosCruza);
