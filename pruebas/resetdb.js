bf830e1bfc328f763e984b52eba8add9c37d7113
Jorge Taborda <taborda.jorge@hotmail.com>
2023-07-11 09:27:29 -0300

    Agregado los turnos

--- pruebas/resetdb.js
+++ pruebas/resetdb.js
@@ -96,22 +96,58 @@ var consulta = {
             }
         })
     },
+
+    agregarHistoria: async function agregarHistoria(historia){
+        return await prisma.historias.create({
+            data:{
+                practica: historia.practica,
+                observaciones: historia.observaciones,
+                dia: historia.dia,
+                monto: historia.monto,
+                cliente: { connect:{ id: parseInt(historia.id_cliente)}},
+                perro: { connect:{ id: parseInt(historia.id_perro)}}
+            }
+        })
+    },
+
+    agregarTurno: async function agregarTurno(turno){
+        return await prisma.turnos.create({
+            data:{
+                practica: turno.practica,
+                dia: turno.dia,
+                franja: turno.franja,
+                estado: turno.estado,
+                cliente: { connect:{ id: parseInt(turno.id_cliente)}},
+                perro: { connect:{ id: parseInt(turno.id_perro)}}
+            }
+        })
+    },
 }
 
 
 async function admins() {
-    return await prisma.administradores.create({
+    await prisma.administradores.create({
         data:{
             nombre:"Pedro",
             apellido: "OhMyDog",
             email: "ohmydogis2@gmail.com",
             password: "12345"
         }
-    })
+    });
+    return await prisma.administradores.create({
+        data:{
+            nombre:"Lucia",
+            apellido: "OhMyDog",
+            email: "admin@lucia.com",
+            password: "123"
+        }
+    });
 }
 
 
 async function delete_all() {
+    await prisma.historias.deleteMany();
+    await prisma.turnos.deleteMany();
     await prisma.donaciones.deleteMany();
     await prisma.publicacion_adopciones.deleteMany();
     await prisma.publicacion_servicios.deleteMany();
@@ -251,6 +287,159 @@ async function main(){
     await consulta.sumarMontoDonacion(campaña_donacion1.id, donacion2.monto);
     console.log('Sumas asignadas a campañas');
 
+
+
+    // ----------- Datos de prueba para TURNOS ----------------
+
+    var datosClienteSinPerros = {
+        nombre: "Cliente",
+        apellido: "SinPerros",
+        email: "cliente@sinperros.com",
+        dni: 11222333,
+        telefono: "2215550000",
+        password: "123",
+        descuento: 0,
+    };
+    var datosClienteConPerros = {
+        nombre: "Cliente",
+        apellido: "ConPerros",
+        email: "cliente@conperros.com",
+        dni: 22333444,
+        telefono: "2215551111",
+        password: "123",
+        descuento: 0,
+    };
+    var datosClienteConPerroYTurnos = {
+        nombre: "Cliente",
+        apellido: "ConPerro YTurnos",
+        email: "taborda.jorge@hotmail.com",
+        dni: 33444555,
+        telefono: "2215552222",
+        password: "123",
+        descuento: 0,
+    };
+    var clienteSinPerros = await consulta.agregarCliente(datosClienteSinPerros);
+    var clienteConPerros = await consulta.agregarCliente(datosClienteConPerros);
+    var clienteConPerroYTurnos = await consulta.agregarCliente(datosClienteConPerroYTurnos);
+
+
+    var datosPerroYaVacunado = {
+        nombre: "Perro YaVacunado",
+        raza: "Mestizo",
+        color: "Gris",
+        fecha_nacimiento: new Date(2020,10,10),
+        link_foto: "",
+        observaciones: "Oreja mordida",
+        clienteId: clienteConPerros.id
+    };
+    var datosPerroMayorCientoVeinte = {
+        nombre: "Perro Mayor CientoVeinte",
+        raza: "Collie",
+        color: "Naranja y Blanco",
+        fecha_nacimiento: new Date(2023,1,1),
+        link_foto: "",
+        observaciones: "Ninguna",
+        clienteId: clienteConPerros.id
+    };
+    var datosPerroMenorCientoVeinte = {
+        nombre: "Perro Menor CientoVeinte",
+        raza: "Chihuahua",
+        color: "Marron",
+        fecha_nacimiento: new Date(2023,4,1),
+        link_foto: "",
+        observaciones: "Tiembla poco para ser un chihuahua",
+        clienteId: clienteConPerros.id
+    };
+    var datosPerroMenorSesenta = {
+        nombre: "Perro Menor Sesenta",
+        raza: "Doberman",
+        color: "Marron y negro",
+        fecha_nacimiento: new Date(2023,6,1),
+        link_foto: "",
+        observaciones: "Muy mansito",
+        clienteId: clienteConPerros.id
+    };
+    var datosPerroConTurnos = {
+        nombre: "Perro ConTurnos",
+        raza: "Pitbull",
+        color: "Blanco y negro",
+        fecha_nacimiento: new Date(2022,6,1),
+        link_foto: "",
+        observaciones: "Muy jugueton",
+        clienteId: clienteConPerroYTurnos.id
+    };
+    var perroYaVacunado = await consulta.agregarPerro(datosPerroYaVacunado);
+    var perroMayorCientoVeinte = await consulta.agregarPerro(datosPerroMayorCientoVeinte);
+    var perroMenorCientoVeinte = await consulta.agregarPerro(datosPerroMenorCientoVeinte);
+    var perroMenorSesenta = await consulta.agregarPerro(datosPerroMenorSesenta);
+    var perroConTurnos = await consulta.agregarPerro(datosPerroConTurnos);
+
+
+    var datosHistoriaPrimerVacunaA = {
+        practica: "Vacuna A",
+        observaciones: "Primera dosis",
+        dia: new Date('2022-01-01'),
+        monto: 200,
+        id_cliente: clienteConPerros.id,
+        id_perro: perroYaVacunado.id
+    };
+    var datosHistoriaSegundaVacunaA = {
+        practica: "Vacuna A",
+        observaciones: "Segunda dosis",
+        dia: new Date('2023-01-01'),
+        monto: 400,
+        id_cliente: clienteConPerros.id,
+        id_perro: perroYaVacunado.id
+    };
+    var datosHistoriaPrimerVacunaB = {
+        practica: "Vacuna B",
+        observaciones: "Primera dosis",
+        dia: new Date('2022-06-01'),
+        monto: 300,
+        id_cliente: clienteConPerros.id,
+        id_perro: perroYaVacunado.id
+    };
+    var datosHistoriaSegundaVacunaB = {
+        practica: "Vacuna B",
+        observaciones: "Segunda dosis",
+        dia: new Date('2023-06-01'),
+        monto: 450,
+        id_cliente: clienteConPerros.id,
+        id_perro: perroYaVacunado.id
+    };
+    var historiaPrimerVacunaA = await consulta.agregarHistoria(datosHistoriaPrimerVacunaA);
+    var historiaSegundaVacunaA = await consulta.agregarHistoria(datosHistoriaSegundaVacunaA);
+    var historiaPrimerVacunaB = await consulta.agregarHistoria(datosHistoriaPrimerVacunaB);
+    var historiaSegundaVacunaB = await consulta.agregarHistoria(datosHistoriaSegundaVacunaB);
+
+
+    var datosTurnoConsulta = {
+        dia: new Date('2023-08-01'),
+        franja: "Tarde",
+        practica: "Consulta",
+        estado: "Pendiente",
+        id_perro: perroConTurnos.id,
+        id_cliente: clienteConPerroYTurnos.id
+    };
+    var datosTurnoOperacion = {
+        dia: new Date('2023-08-01'),
+        franja: "Tarde",
+        practica: "Operacion",
+        estado: "Pendiente",
+        id_perro: perroConTurnos.id,
+        id_cliente: clienteConPerroYTurnos.id
+    };
+    var datosTurnoPeluqueria = {
+        dia: new Date('2023-08-01'),
+        franja: "Tarde",
+        practica: "Peluqueria",
+        estado: "Aceptado",
+        id_perro: perroConTurnos.id,
+        id_cliente: clienteConPerroYTurnos.id
+    };
+    var turnoConsulta = await consulta.agregarTurno(datosTurnoConsulta);
+    var turnoOperacion = await consulta.agregarTurno(datosTurnoOperacion);
+    var turnoPeluqueria = await consulta.agregarTurno(datosTurnoPeluqueria);
 }
 
 main();
--- src/controllers/turnoController.js
+++ src/controllers/turnoController.js
@@ -1,11 +1,233 @@
-const db = require('../models/turnoDB')
+const dbTurno = require('../models/turnoDB');
+const mailer = require('../../mail');
 
 module.exports = {
     solicitar: async (req,res) => {
+        var cliente = await dbTurno.clienteConPerros(req.session.usuario);
+        var error = "";
+        if(cliente.perros.length == 0)
+            error = "Debe tener un perro registrado para poder solicitar un turno";
+
         res.render('turno/solicitar', {
             title: 'Solicitar turno',
             message: 'Solicitar turno',
-            usuario: req.session
+            usuario: req.session,
+            perros: cliente.perros,
+            error: error
         });
-    }
+    },
+    
+    intento: async (req, res) => {
+        var fechaHoy = new Date();
+        var fechaTurno = new Date(req.body.dia);
+        var cliente = await dbTurno.clienteConPerros(req.session.usuario);
+        var cantVacunasA = await dbTurno.cantVacunasA(req.body.perro);
+        var cantVacunasB = await dbTurno.cantVacunasB(req.body.perro);
+        var tieneTurnoVacunaA = await dbTurno.tieneTurnoVacunaA(req.body.perro);
+        var tieneTurnoVacunaB = await dbTurno.tieneTurnoVacunaB(req.body.perro);
+        var perro = await dbTurno.verPerro(req.body.perro);
+        var edadEnDias = (fechaHoy - perro.fecha_nacimiento) / 86400000;
+
+
+        var error = "";
+        if(req.body.practica == "Vacuna B"){
+            if(cantVacunasB == 2) error = "El perro ya ha sido vacunado con Vacuna B";
+            else if(cantVacunasB == 1 && tieneTurnoVacunaB) error = "Ya dispone de un turno para la segunda dosis de Vacuna B";
+            else if(cantVacunasB == 0 && edadEnDias < 120) error = "El perro debe tener al menos 4 meses para poder aplicarle la Vacuna B";
+        }
+        if(req.body.practica == "Vacuna A"){
+            if(cantVacunasA == 2) error = "El perro ya ha sido vacunado con Vacuna A";
+            else if(cantVacunasA == 1 && tieneTurnoVacunaA) error = "Ya dispone de un turno para la segunda dosis de Vacuna A";
+            else if(cantVacunasA == 0 && edadEnDias < 60) error = "El perro debe tener al menos 2 meses para poder aplicarle la Vacuna A";
+        }
+        if(fechaTurno <= fechaHoy) error = "Debe solicitar un turno a partir de la fecha de mañana";
+
+
+        if(error != ""){
+            res.render('turno/solicitar', {
+                title: 'Solicitar turno',
+                message: 'Solicitar turno',
+                usuario: req.session,
+                perros: cliente.perros,
+                error: error
+            });
+        }
+        else{
+            var nuevoTurno = {
+                id_perro: req.body.perro,
+                practica: req.body.practica,
+                franja: req.body.franja,
+                dia: fechaTurno,
+                id_cliente: req.session.usuario
+            };
+            await dbTurno.crearTurno(nuevoTurno);
+            var info = "Turno solicitado correctamente";
+            var turnos = await dbTurno.listarTurnos(req.session.usuario);
+            turnos.forEach(turno => turno.dia.setTime(turno.dia.getTime() + 10800000));
+            res.render('turno/listar', {
+                title: 'Mis Turnos',
+                message: 'Mis Turnos',
+                usuario: req.session,
+                turnos: turnos,
+                info: info
+            });
+        }
+    },
+
+    listar: async (req,res) => {
+        var turnos = await dbTurno.listarTurnos(req.session.usuario);
+        if(turnos.length == 0)
+            var info = "Aún no ha solicitado ningún turno";
+        else{
+            turnos.forEach(turno => turno.dia.setTime(turno.dia.getTime() + 10800000));
+        }
+
+        res.render('turno/listar', {
+            title: 'Mis Turnos',
+            message: 'Mis Turnos',
+            usuario: req.session,
+            turnos: turnos,
+            info: info
+        });
+    },
+
+    cancelar: async (req,res) => {
+        await dbTurno.cancelarTurno(req.params.id);
+        var turnos = await dbTurno.listarTurnos(req.session.usuario);
+        turnos.forEach(turno => turno.dia.setTime(turno.dia.getTime() + 10800000));
+        var info = "Turno cancelado exitosamente";
+
+        res.render('turno/listar', {
+            title: 'Mis Turnos',
+            message: 'Mis Turnos',
+            usuario: req.session,
+            turnos: turnos,
+            info: info
+        });
+    },
+
+    listarAdmin: async (req,res) => {
+        var fecha = new Date();
+        if(req.body.dia){
+            fecha = new Date(req.body.dia);
+            fecha.setTime(fecha.getTime() + 10800000);
+        }
+
+        var turnos = await dbTurno.listarTurnosFecha(fecha);
+        if(turnos.length == 0)
+            var info = "Aún no han solicitado ningún turno para este día";
+        else{
+            turnos.forEach(turno => turno.dia.setTime(turno.dia.getTime() + 10800000));
+        }
+
+        res.render('turno/listarAdmin', {
+            title: 'Listado de Turnos',
+            message: 'Listado de Turnos '+fecha.toLocaleDateString(),
+            usuario: req.session,
+            turnos: turnos,
+            fecha: fecha,
+            info: info
+        });
+    },
+
+    aceptar: async (req,res) => {
+        await dbTurno.aceptarTurno(req.params.id);
+        var info = "Turno aceptado exitosamente";
+        var turno = await dbTurno.verDetalle(req.params.id);
+        var fecha = turno.dia;
+
+        if(!fecha)
+            fecha = new Date();
+        fecha.setTime(fecha.getTime() + 10800000);
+
+        var turnos = await dbTurno.listarTurnosFecha(fecha);
+        if(turnos.length >= 0)
+            turnos.forEach(turno => turno.dia.setTime(turno.dia.getTime() + 10800000));
+
+        
+        if(turno.franja == "Temprano")
+            var franja = "de 8 a 13hs";
+        else
+            var franja = "de 15 a 20hs";
+        mailer.sendMail(turno.cliente.email,'Turno aceptado OhMyDog!',"Tu solicitud de turno en la veterinaria OhMyDog! fue aceptado para el día " + fecha + " y la franja horaria " + franja);
+        
+
+        res.render('turno/listarAdmin', {
+            title: 'Listado de Turnos',
+            message: 'Listados de Turnos '+fecha.toLocaleDateString(),
+            usuario: req.session,
+            turnos: turnos,
+            fecha: fecha,
+            info: info
+        });
+    },
+
+    rechazar: async (req,res) => {
+        await dbTurno.rechazarTurno(req.params.id);
+        var info = "Turno rechazado exitosamente";
+        var turno = await dbTurno.verDetalle(req.params.id);
+        var fecha = turno.dia;
+
+        if(!fecha)
+            fecha = new Date();
+        fecha.setTime(fecha.getTime() + 10800000);
+
+        var turnos = await dbTurno.listarTurnosFecha(fecha);
+        if(turnos.length >= 0)
+            turnos.forEach(turno => turno.dia.setTime(turno.dia.getTime() + 10800000));
+
+        
+        if(turno.franja == "Temprano")
+            var franja = "de 8 a 13hs";
+        else
+            var franja = "de 15 a 20hs";
+        mailer.sendMail(turno.cliente.email,'Turno rechazado OhMyDog!',"Tu solicitud de turno en la veterinaria OhMyDog! fue rechazado para el día " + fecha + " y la franja horaria " + franja + ". Por favor vuelva a solicitar un turno.");
+        
+
+        res.render('turno/listarAdmin', {
+            title: 'Listado de Turnos',
+            message: 'Listados de Turnos '+fecha.toLocaleDateString(),
+            usuario: req.session,
+            turnos: turnos,
+            fecha: fecha,
+            info: info
+        });
+    },
+
+    verDetalle: async (req,res) => {
+        var turno = await dbTurno.verDetalle(req.params.id);
+        turno.dia.setTime(turno.dia.getTime() + 10800000);
+        turno.dia = turno.dia.toLocaleDateString();
+
+        res.render('turno/verDetalle', {
+            title: 'Detalle Turno',
+            message: 'Detalle del Turno',
+            usuario: req.session,
+            turno: turno
+        });
+    },
+
+    listarAceptadosAdmin: async (req,res) => {
+        var fecha = new Date();
+        if(req.body.dia){
+            fecha = new Date(req.body.dia);
+            fecha.setTime(fecha.getTime() + 10800000);
+        }
+
+        var turnos = await dbTurno.listarTurnosAceptadosFecha(fecha);
+        if(turnos.length == 0)
+            var info = "Aún no hay turnos para este día";
+        else{
+            turnos.forEach(turno => turno.dia.setTime(turno.dia.getTime() + 10800000));
+        }
+
+        res.render('turno/listarAceptadosAdmin', {
+            title: 'Turnos Aceptados',
+            message: 'Listado de Turnos Aceptados '+fecha.toLocaleDateString(),
+            usuario: req.session,
+            turnos: turnos,
+            fecha: fecha,
+            info: info
+        });
+    },
 }
